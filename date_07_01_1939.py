c4eeab2653415a99761922db58805e86f661a538
Batruha1488 <150271889+Batruha1488@users.noreply.github.com>
2024-02-19 18:08:39 +0200

    Add files via upload

---
+++ date_07_01_1939.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import tkinter .ttk as ttk
+import asyncio
+import datetime
+import python_weather
+import os
+
+async def getweather():
+  
+  async with python_weather.Client(unit=python_weather.METRIC) as client:
+    weather = await client.get('Chuhuiv')
+    
+    return weather
+
+
+
+class Weather(ttk.Frame):
+    
+    def __init__(self,master,*args,**kwargs):
+        super().__init__(master)
+        self.weather_field = ttk.Label(self,text = "14:88") 
+        self.weather_field.pack()
+    async def run(self):
+        try:
+            weather_task = asyncio.create_task(getweather())
+            weather = await weather_task
+            self.weather_field["text"] = str(weather)
+        except:
+            pass
